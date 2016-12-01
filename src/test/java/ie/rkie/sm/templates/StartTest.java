df12fe82a3e60994e2cae4abc1cca3e35ccfe7ae
rkie <rkie@users.noreply.github.com>
2016-12-01 06:33:05 +0000

    Adding test for the start page

---
+++ src/test/java/ie/rkie/sm/templates/StartTest.java
@@ -0,0 +1,53 @@
+package ie.rkie.sm.templates;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StartTest {
+
+    @Autowired
+    private WebApplicationContext context;
+    
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private MessageSource messageSource;
+    
+    @Before
+    public void before() {
+    	mockMvc = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity())
+				.build();
+    }
+
+	@Test
+	@WithUserDetails(value="bob", userDetailsServiceBeanName="userDetailsService")
+	public void testLoginGet() throws Exception {    	
+		mockMvc.perform(get("/start"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("<a data-toggle=\"tab\" href=\"#chess\">Chess</a>")))
+			.andExpect(content().string(containsString("<a data-toggle=\"tab\" href=\"#draughts\">Draughts</a>")))
+			.andExpect(content().string(containsString("<a data-toggle=\"tab\" href=\"#snakes\">Snakes and Ladders</a>")));
+	}
+}
